1dcedaf1d5c4dfb2ce6d941ac53d978f77e52c32
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-18 19:58:18 +0600

    qzones images center align

--- src/components/homelayout/Qzone.jsx
+++ src/components/homelayout/Qzone.jsx
@@ -6,7 +6,7 @@ const QZone = () => {
   return (
     <div className="bg-base-200 p-3">
       <h2 className="font-bold mb-5">QZone</h2>
-      <div className="space-y-5">
+      <div className="space-y-5 flex flex-col items-center">
         <img src={swimmingImage} alt="" />
         <img src={classImage} alt="" />
         <img src={playImage} alt="" />
